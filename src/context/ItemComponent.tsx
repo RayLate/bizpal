f14472a203117e1beb0d36ee5a8d1a43d362cb77
Anbang Li <richardlab522@gmail.com>
2023-04-01 10:15:05 +0800

    update files into ts

---
+++ src/context/ItemComponent.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { useItem } from './ItemContext';
+import { sendAPICall } from './api';
+
+const ItemComponent: React.FC = () => {
+  const { data, setData } = useItem();
+  // Add any additional state variables and UI elements you need
+
+  const handleButtonClick = async () => {
+    const url = 'https://7beqwqk0rk.execute-api.us-east-1.amazonaws.com/prod/items';
+    const httpMethod = 'GET'; // Replace with your desired HTTP method
+    const response = await sendAPICall(url, httpMethod, data);
+    console.log(response);
+  };
+
+  return (
+    <div>
+      {/* Render your UI components here */}
+      <button onClick={handleButtonClick}>Send Request</button>
+    </div>
+  );
+};
+
+export default ItemComponent;
